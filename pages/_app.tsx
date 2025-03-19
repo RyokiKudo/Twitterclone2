a85cd6a0b8a105f967951af5c6b2310f3dc5cc95
RyokiKudo <zenoninfinity46@gmail.com>
2025-03-20 04:07:35 +0900

    Modalコンポーネントを実装し、アプリ全体で表示できるように統合。Headerコンポーネントにバックアロー表示オプションを追加。

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import Layout from "../components/Layout";
 import "@/styles/globals.css";
+import Modal from "components/Modal";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
+  <>
+    <Modal isOpen/>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </>
   )
 }
