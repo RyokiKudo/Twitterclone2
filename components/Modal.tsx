8920547c8dc40a86c0a708f9c252648805bf9d1b
RyokiKudo <zenoninfinity46@gmail.com>
2025-03-20 03:49:13 +0900

    Modalコンポーネントを新規作成し、基本的なプロパティとハンドラを追加

---
+++ components/Modal.tsx
@@ -0,0 +1,44 @@
+import React, { use, useCallback } from "react";
+
+interface ModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+    onSubmit: () => void;
+    title: string;
+    body: React.ReactElement;
+    actionLabel: string;
+    disabled?: boolean;
+}
+
+const Modal:React.FC<ModalProps> = ({
+    isOpen,
+    onClose,
+    onSubmit,
+    title,
+    body,
+    actionLabel,
+    disabled
+}) => {
+    const handleClose = useCallback(() => {
+        if (disabled) {
+            return;
+        }
+
+        onClose();
+    }, [disabled, onClose]);
+
+    const handleSubmit = useCallback(() => {
+        if (disabled) {
+            return;
+        }
+
+        onSubmit();
+    }, [disabled, onSubmit]);
+
+    if (!isOpen) {
+        return null;
+    }
+    return(<div></div>);
+}
+
+export default Modal;
