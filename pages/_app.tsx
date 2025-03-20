605e6cbce174e911079dbcdaa2badfcaaa8ddfb0
RyokiKudo <zenoninfinity46@gmail.com>
2025-03-20 16:16:57 +0900

    Modalコンポーネントにタイトルプロパティを追加し、アプリ全体での表示を改善

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
+import type { AppProps } from "next/app";
+
 import Layout from "../components/Layout";
 import "@/styles/globals.css";
 import Modal from "components/Modal";
-import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
   <>
-    <Modal isOpen/>
+    <Modal isOpen title="Test Modal" />
     <Layout>
       <Component {...pageProps} />
     </Layout>
