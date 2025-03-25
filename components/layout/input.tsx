b1cf7175801c4b36c9a72fc85db819ad00aad55c
RyokiKudo <zenoninfinity46@gmail.com>
2025-03-26 02:06:20 +0900

    Modalコンポーネントのアクションラベルを削除し、Inputコンポーネントを新規作成

---
+++ components/layout/input.tsx
@@ -0,0 +1,8 @@
+const input = () => {
+    return (
+        <div>
+        <input type="text" placeholder="Enter text here" />
+        </div>
+    );
+};
+export default input;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,11 @@ import type { AppProps } from "next/app";
 
 import Layout from "../components/Layout";
 import "@/styles/globals.css";
-import Modal from "components/Modal";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
   <>
-    <Modal actionLabel="ツイートする" isOpen title="いまどうしてる？" />
+    
     <Layout>
       <Component {...pageProps} />
     </Layout>
