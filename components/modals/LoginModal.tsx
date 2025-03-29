86d130eb01987ed3b4ee87a6061e37fc4c2c79cf
RyokiKudo <zenoninfinity46@gmail.com>
2025-03-29 10:02:41 +0900

    LoginModalコンポーネントにuseLoginModalフックを追加し、メールとパスワードの状態管理を実装

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -1,4 +1,15 @@
+import useLoginModal from "@/hooks/useLoginModal";
+import { useState } from "react";
+
 const LoginModal = () => {
+    const loginModal = useLoginModal();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isOpenLoading, setIsOpenLoading] = useState(false);
+
+    
+
     return (
         <div></div>
     );
