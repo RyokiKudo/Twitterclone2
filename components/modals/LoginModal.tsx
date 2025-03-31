fa6a270a677c66f0763d640d3a0bb32403746e5c
RyokiKudo <zenoninfinity46@gmail.com>
2025-03-31 21:54:17 +0900

    Inputコンポーネントを新規作成し、LoginModalコンポーネントで使用するように修正

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -1,20 +1,45 @@
 import useLoginModal from "@/hooks/useLoginModal";
 import { use, useCallback, useState } from "react";
+import Input from "components/Input";
 
 const LoginModal = () => {
     const loginModal = useLoginModal();
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [isOpenLoading, setIsOpenLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
     const onsubmit = useCallback(async() => {
         try {
+            setIsLoading(true);
 
+            // Todo: Add login
+
+            
+        loginModal.onClose();
         } catch (error) {
             console.log(error);
+        } finally {
+            setIsLoading(false);
         }
-    },[]);
+    },[loginModal]);
+
+    const bodyContent = (
+        <div className="flex flex-col gap-4">
+            <Input
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                disabled={isLoading} 
+            />
+            <Input
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                disabled={isLoading} 
+            />
+        </div>
+    );
 
     return (
         <div></div>
